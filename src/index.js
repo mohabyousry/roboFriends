33fe36979babb841e53d2b62a3b46168e3f9df26
mohabyousry <mohabyousry@outlook.com>
2023-10-24 21:41:10 +0300

    uploading project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "tachyons";
+import reportWebVitals from "./reportWebVitals";
+import App from "./containers/App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
